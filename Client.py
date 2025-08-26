0a7878959508f244f7f1ec9ae3a417a18c0d5e14
fethidenker <fethidenker@sabanciuniv.edu>
2025-08-26 20:34:46 +0100

    Cyber Security Project

---
+++ Client.py
@@ -0,0 +1,94 @@
+import math
+import time
+import random
+import sympy
+import warnings
+from random import randint, seed
+import sys
+from ecpy.curves import Curve,Point
+from Crypto.Hash import SHA3_256, HMAC, SHA256
+import requests
+from Crypto.Cipher import AES
+from Crypto import Random
+from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import unpad
+import random
+import re
+import json
+
+API_URL = 'http://harpoon1.sabanciuniv.edu:9999'
+
+stuID = 27804
+
+E = Curve.get_curve("secp256k1")
+n = E.order
+P = E.generator
+
+
+IKey_Pr = 16583104134778130762479186827572996124244097953595394773612560901170604126940
+IKey_Pub = Point(42962279252123192900230332240901450736038335793155372586682863834229270830844, 2141846162555728367847099143914084174553403144481850923314167063833809172935, E)
+
+
+
+#Server's Identitiy public key
+IKey_Ser = (
+    0x1d42d0b0e55ccba0dd86df9f32f44c4efd7cbcdbbb7f36fd38b2ca680ab126e9,
+    0xce091928fa3738dc18f529bf269ade830eeb78672244fd2bdfbadcb26c4894ff,
+)  # Use the values in the project description document to form the server's IK as a point on the EC. Note that the values should be in decimal.
+
+def IKRegReq(h,s,x,y):
+    mes = {'ID':stuID, 'H': h, 'S': s, 'IKPUB.X': x, 'IKPUB.Y': y}
+    print("Sending message is: ", mes)
+    response = requests.put('{}/{}'.format(API_URL, "IKRegReq"), json = mes)		
+    print(response.json())
+
+def IKRegVerify(code):
+    mes = {'ID':stuID, 'CODE': code}
+    print("Sending message is: ", mes)
+    response = requests.put('{}/{}'.format(API_URL, "IKRegVerif"), json = mes)
+    if((response.ok) == False): raise Exception(response.json())
+    else:
+        print(response.json())
+        f = open('Identity_Key.txt', 'w')
+        f.write("IK.Prv: "+str(IKey_Pr)+"\n"+"IK.Pub.x: "+str(IKey_Pub.x)+"\n"+"IK.Pub.y: "+str(IKey_Pub.y))
+        f.close()
+
+def SPKReg(h,s,x,y):
+    mes = {'ID':stuID, 'H': h, 'S': s, 'SPKPUB.X': x, 'SPKPUB.Y': y}
+    print("Sending message is: ", mes)
+    response = requests.put('{}/{}'.format(API_URL, "SPKReg"), json = mes)
+    print(response.json())			
+    
+
+def OTKReg(keyID,x,y,hmac):
+    mes = {'ID':stuID, 'KEYID': keyID, 'OTKI.X': x, 'OTKI.Y': y, 'HMACI': hmac}
+    print("Sending message is: ", mes)
+    response = requests.put('{}/{}'.format(API_URL, "OTKReg"), json = mes)		
+    print(response.json())
+    if((response.ok) == False): return False
+    else: return True
+
+
+def ResetIK(rcode):
+    mes = {'ID':stuID, 'RCODE': rcode}
+    print("Sending message is: ", mes)
+    response = requests.delete('{}/{}'.format(API_URL, "ResetIK"), json = mes)		
+    print(response.json())
+    if((response.ok) == False): return False
+    else: return True
+
+def ResetSPK(h,s):
+    mes = {'ID':stuID, 'H': h, 'S': s}
+    print("Sending message is: ", mes)
+    response = requests.delete('{}/{}'.format(API_URL, "ResetSPK"), json = mes)		
+    print(response.json())
+    if((response.ok) == False): return False
+    else: return True
+
+
+def ResetOTK(h,s):
+    mes = {'ID':stuID, 'H': h, 'S': s}
+    print("Sending message is: ", mes)
+    response = requests.delete('{}/{}'.format(API_URL, "ResetOTK"), json = mes)		
+    if((response.ok) == False): print(response.json())
+
